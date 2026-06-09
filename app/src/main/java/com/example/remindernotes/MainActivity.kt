42a395523d11045ada1c4ba6347b344b84d84715
Kirill <kiruxa.37v@gmail.com>
2026-06-09 22:07:31 +0300

    clear all 🥹

--- app/src/main/java/com/example/remindernotes/MainActivity.kt
+++ app/src/main/java/com/example/remindernotes/MainActivity.kt
@@ -1,21 +1,13 @@
 package com.example.remindernotes
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.remindernotes.data.viewmodels.NoteViewModel
-import com.example.remindernotes.data.viewmodels.NoteViewModelFactory
-import com.example.remindernotes.databinding.ActivityMainBinding
-import com.example.remindernotes.ui.list.NoteAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Logger.init(this)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        setContentView(R.layout.activity_main)
     }
 }
